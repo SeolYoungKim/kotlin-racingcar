44b67564a8f52792a9bfce0ced79620cdcd73994
SeolYoungKim <nasur4da@gmail.com>
2024-11-23 12:12:10 +0900

    test: 결과 문자열에 자동차 이름이 함께 나타나는 테스트 작성

--- src/test/kotlin/racingcar/domain/ResultGeneratorTest.kt
+++ src/test/kotlin/racingcar/domain/ResultGeneratorTest.kt
@@ -8,18 +8,18 @@ class ResultGeneratorTest {
     fun `자동차 목록을 받아 각 자동차의 position 에 따라 결과 문자열을 생성한다`() {
         val cars =
             listOf(
-                Car(position = 1, name = "김설영"),
-                Car(position = 2, name = "김설영"),
-                Car(position = 3, name = "김설영"),
+                Car(position = 1, name = "김씨"),
+                Car(position = 2, name = "박씨"),
+                Car(position = 3, name = "이씨"),
             )
 
         val result = ResultGenerator.generate(cars)
 
         assertThat(result).isEqualTo(
             """
-            -
-            --
-            ---
+            김씨 : -
+            박씨 : --
+            이씨 : ---
             
             """.trimIndent(),
         )
