1d4d0ece50a6b6f5a4b241c551ae3ef691449006
SeolYoungKim <nasur4da@gmail.com>
2024-11-23 15:07:33 +0900

    test: 이름 목록을 받아 자동차를 만드는 테스트 작성

--- src/test/kotlin/racingcar/domain/CarFactoryTest.kt
+++ src/test/kotlin/racingcar/domain/CarFactoryTest.kt
@@ -10,4 +10,13 @@ class CarFactoryTest {
 
         assertThat(cars).hasSize(10)
     }
+
+    @Test
+    fun `이름 목록을 입력받아 자동차를 만든다`() {
+        val names = listOf("김씨", "이씨", "박씨")
+
+        val cars = CarFactory.create(names)
+
+        assertThat(cars).hasSize(names.size)
+    }
 }
